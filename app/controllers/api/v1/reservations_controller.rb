d2b4c6faba8877928f02893a30a1b064b2be310f
cmonkey03 <russellpbrown@gmail.com>
2018-10-19 13:36:27 -0400

    Added routes and controllers

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::ReservationsController < ApplicationController
+  before_action :find_reservation, only: [:destroy]
+
+  def index
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
+  private
+
+    def reservation_params
+      params.permit(:id, :user_id, :tour_id)
+    end
+
+    def find_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :find_user, only: [:update]
+
+  def index
+    @users = User.all
+    render json: @users, status: 200
+  end
+
+  
+
+  private
+
+    def user_params
+      params.permit(:username, :password, :email, :zipcode, :bio)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
 end
