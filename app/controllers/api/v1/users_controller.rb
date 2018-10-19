519450edcc18e76a8ec6e96bfcc2b229442f3738
cmonkey03 <russellpbrown@gmail.com>
2018-10-19 17:30:39 -0400

    Server-side JWT Auth complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
   before_action :find_user, only: [:update]
 
   def index
     @users = User.all
     render json: @users, status: 200
+  end
+
+  def profile
+    render json: { user: UserSerializer.new(curr_user) }, status: :accepted
   end
 
   def create
