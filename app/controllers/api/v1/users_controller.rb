cdcc3fa69f27935affa0c5495079ab6acb0d7338
cmonkey03 <russellpbrown@gmail.com>
2018-10-19 14:10:15 -0400

    Added User create function

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,14 @@ class Api::V1::UsersController < ApplicationController
     render json: @users, status: 200
   end
 
-  
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
 
   private
 
