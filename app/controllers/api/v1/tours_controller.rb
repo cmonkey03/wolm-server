bc894c896828fe475cc249079816d3aa85b986e7
cmonkey03 <russellpbrown@gmail.com>
2018-10-23 19:16:16 -0400

    Add create to ToursController

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -7,11 +7,29 @@ class Api::V1::ToursController < ApplicationController
     render json: @tours
   end
 
-  private
-
-    def tour_params
-      params.require(:tour).permit(:start_time, :end_time, :price)
+  def create
+    @tour = Tour.create(tour_params)
+    if @tour.valid?
+      render json: @tour, status: :accepted
+    else
+      render json: { errors: @tour.errors.full_messages }, status: :unprocessible_entity
     end
+  end
+
+  # def update
+  #   @tour.update(tour_params)
+  #   if @tour.save
+  #     render json: @tour, status: :accepted
+  #   else
+  #     render json: { errors: @tour.errors.full_messages }, status: :unprocessible_enttity
+  #   end
+  # end
+  #
+  # private
+  #
+  #   def tour_params
+  #     params.require(:tour).permit(:start_time, :end_time, :price)
+  #   end
 
     # def find_tour
     #   @tour = Tour.find(params[:id])
