97f6ea24272db19ae3dcdbe12a5dc136df07a94d
github username <github email address>
2019-02-18 19:28:58 -0500

    Removed image_url from tour serializer

--- app/serializers/tour_serializer.rb
+++ app/serializers/tour_serializer.rb
@@ -1,5 +1,5 @@
 class TourSerializer < ActiveModel::Serializer
-  attributes :id, :start_time, :end_time, :price, :image_url, :description
+  attributes :id, :start_time, :end_time, :price, :description
   has_many :reservations
   has_many :users, through: :reservations
 end
