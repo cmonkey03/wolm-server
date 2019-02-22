0299cef2cb01456376ba50505c0d472240629ab9
github username <github email address>
2019-02-21 19:09:07 -0500

    updated params order

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::ToursController < ApplicationController
   private
 
     def tour_params
-      params.require(:tour).permit(:start_time, :end_time, :description, :price)
+      params.require(:tour).permit(:start_time, :end_time, :price, :description)
     end
 
     # def find_tour
